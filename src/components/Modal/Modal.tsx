1a3f9e0fe57f6757e3f3566ce576d8a50cc81c1f
Nikolay Stoev <nikolay@liveplanet.net>
2019-07-15 14:29:09 -0700

    body scroll hook added

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ReactNode } from 'react';
-import useLockBodyScroll from 'hooks/useLockBodyScroll';
+import useLockBodyScroll from '../../hooks/useLockBodyScroll';
 import css from './Modal.module.scss';
 
 export interface ModalProps {
